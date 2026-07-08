bb8ec5b9e4f1e5d7e27cb8b8fb9b884e900797ed
kkamal11 <kishorkamal7091@gmail.com>
2026-07-08 08:31:14 +0530

    encode-decode-json

--- myjson/main.go
+++ myjson/main.go
@@ -24,14 +24,45 @@ func main() {
 	}
 	jsonString := EncodeJson(courses)
 	fmt.Println(jsonString)
+	fmt.Println("==================================")
+	isJsonValid := json.Valid([]byte(jsonString))
+	if isJsonValid {
+		fmt.Println("JSON is valid")
+	} else {
+		fmt.Println("JSON is not valid")
+		panic("invalidJson")
+	}
+	originalCourses, originalJsonMap := DecodeJson(jsonString)
+	fmt.Println(originalCourses)
+	fmt.Println(originalJsonMap)
 }
 
 func EncodeJson(courses []Course) string {
 	// code to encode the courses slice into JSON string
-	finalJson, err := json.Marshal(courses)
+	// finalJson, err := json.Marshal(courses)
+	finalJson, err := json.MarshalIndent(courses, "", "  ") //for indentation
 	if err != nil {
 		panic(err)
 	}
 	
 	return string(finalJson)
+}
+
+
+func DecodeJson(jsonData string) ([]Course, []map[string]interface{}) {
+	// code to decode the JSON string into courses slice
+	var courses []Course
+	var err error
+	// err = json.Unmarshal([]byte(jsonData), &courses)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	var myJsonMap []map[string]any
+	err = json.Unmarshal([]byte(jsonData), &myJsonMap)
+	if err != nil {
+		panic(err)
+	}
+	
+	return courses, myJsonMap
 }
