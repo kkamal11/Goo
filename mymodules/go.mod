6622fe75e69a9315f86f0635ac85978daa7d85fe
kkamal11 <kishorkamal7091@gmail.com>
2026-07-09 08:48:25 +0530

    go-mod

---
+++ mymodules/go.mod
@@ -0,0 +1,5 @@
+module github.com/kkamal11/mymodules
+
+go 1.26.4
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ mymodules/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Hello Modules")
+	greeter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+
+func greeter(){
+	fmt.Println("Hello go mod users")
+}
+
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello there</h1>"))
+	fmt.Fprintf(w, "Welcome to the Home Page!")
+	fmt.Println("Endpoint Hit: homePage")
+}
